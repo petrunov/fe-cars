773d2273c983a9b158fbe80c5d53a7fa62a68ca1
Vesselin Petrunov <petrunov@protonmail.com>
2024-07-09 07:20:20 +0300

    Removed unused code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-// import { useState } from 'react';
-import { Button } from '@mui/material';
 import './App.css';
 import CustomTable from './components/CustomTable/CustomTable';
 
 function App() {
-  return (
-    <>
-      <CustomTable></CustomTable>
-      <Button
-        variant="contained"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      ></Button>
-    </>
-  );
+  return <CustomTable />;
 }
 
 export default App;
